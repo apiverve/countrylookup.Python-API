06165c19e234e49ae08b27fe2333e988e0c5a5c9
APIVerve Team <hello@apiverve.com>
2025-03-28 17:12:13 -0500

    Bug Fixes

--- src/apiverve_countrylookup/apiClient.py
+++ src/apiverve_countrylookup/apiClient.py
@@ -10,10 +10,10 @@ class CountrylookupAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/countrylookup'
+        self.base_url = 'https://api.apiverve.com/v1/countrylookup'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
